456680368eb4f0605f1c0233d540bdb092c3f4d2
MaTuna01 <ma775100@gmail.com>
2026-09-13 15:38:38 +0900

    [#3] - react-router-dom 및 react query 패키지 설치

---
+++ src/pages/login/ui/LoginPage.tsx
@@ -0,0 +1,15 @@
+export default function LoginPage() {
+    return (
+        <>
+            <h1>Raily</h1>
+            <h2>앉아서 갈 수 있는 자리를 먼저 찾아드립니다.</h2>
+            <br/>
+            <p>이메일</p>
+            <input type="text"/>
+            <p>비밀번호</p>
+            <input type="text"/>
+            <button>로그인</button>
+            <p>계정이 없으신가요? 회원가입</p>
+        </>
+    )
+}
