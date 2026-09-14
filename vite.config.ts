50ce611cb034e6c3151beb44ec010b3279be24cd
MaTuna01 <ma775100@gmail.com>
2026-09-14 21:45:46 +0900

    [#15] - 요청 주소 오탈자 수정

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       //api 로 시작하는 요청을 모두 백엔드로 넘김
       '/api':{
         // 백엔드 주소는 추후 배포 혹은 도메인 연결시 수정
-        target: 'http://localhost:8000',
+        target: 'http://localhost:8080',
         changeOrigin: true,
       }
     }
