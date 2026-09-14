edb48ba4a68b4cd21bb833068f4874fb715ee582
MaTuna01 <ma775100@gmail.com>
2026-09-14 21:26:57 +0900

    [#15] - 구현된 페이지 라우트 등록

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,19 +2,18 @@ import {Route, Routes} from "react-router-dom"
 import LoginPage from "../pages/login/ui/LoginPage.tsx";
 import SignupPage from "../pages/signup/ui/SignupPage.tsx";
 import {ScreenShell} from "../widgets/screen-shell/ui/ScreenShell.tsx";
+import {JourneySetupPage} from "../pages/journey-setup";
+import {TrainSelectPage} from "../pages/train-select";
 
 export default function App() {
     return (
-        <div>
-            <main>
-                <Routes>
-                    <Route path="/" element={<ScreenShell/>}>
-                        <Route index element={<LoginPage/>}/>
-                        <Route path="sign-up" element={<SignupPage/>}/>
-                    </Route>
-
-                </Routes>
-            </main>
-        </div>
+        <Routes>
+            <Route path="/" element={<ScreenShell/>}>
+                <Route index element={<LoginPage/>}/>
+                <Route path="sign-up" element={<SignupPage/>}/>
+                <Route path="journey-setup" element={<JourneySetupPage/>}/>
+                <Route path="train-slect" element={<TrainSelectPage/>}/>
+            </Route>
+        </Routes>
     )
 }
