3de63f1aae9834cdb804b0be65414a37ae754d22
MaTuna01 <ma775100@gmail.com>
2026-09-14 16:59:59 +0900

    [#11] 로그인 구현을 위한 요청/응답 요청 인터페이스 구현

--- src/entities/user/model/types.ts
+++ src/entities/user/model/types.ts
@@ -3,3 +3,13 @@ export type User = {
     email: string;
     nickname: string;
 };
+
+export interface LoginRequest {
+    email: string;
+    password: string;
+}
+
+export interface LoginResponse {
+    accessToken: string;
+    user: User;
+}
