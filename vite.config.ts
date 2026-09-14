78987b4eb775ab9c712c784d4aa0b199dc6740a2
MaTuna01 <ma775100@gmail.com>
2026-09-14 21:40:39 +0900

    [#15] - vite 리버스 프록시 경로 적용(요청 백엔드 전달)

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      //api 로 시작하는 요청을 모두 백엔드로 넘김
+      '/api':{
+        // 백엔드 주소는 추후 배포 혹은 도메인 연결시 수정
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      }
+    }
+  }
 })
