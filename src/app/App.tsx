e346456ed3f23f336867b56458b3f6e45319df21
MaTuna01 <ma775100@gmail.com>
2026-09-14 21:38:16 +0900

    [#15] - 라우트 경로를 ROUTE 상수로 통일 하고 seat-matrix 라우팅 등록

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,15 +4,18 @@ import SignupPage from "../pages/signup/ui/SignupPage.tsx";
 import {ScreenShell} from "../widgets/screen-shell/ui/ScreenShell.tsx";
 import {JourneySetupPage} from "../pages/journey-setup";
 import {TrainSelectPage} from "../pages/train-select";
+import SeatMatrixPage from "../pages/seat-matrix/ui/SeatMatrixPage.tsx";
+import {ROUTES} from "../shared/config/routes.ts";
 
 export default function App() {
     return (
         <Routes>
-            <Route path="/" element={<ScreenShell/>}>
-                <Route index element={<LoginPage/>}/>
-                <Route path="sign-up" element={<SignupPage/>}/>
-                <Route path="journey-setup" element={<JourneySetupPage/>}/>
-                <Route path="train-selct" element={<TrainSelectPage/>}/>
+            <Route element={<ScreenShell />}>
+                <Route path={ROUTES.LOGIN} element={<LoginPage />} />
+                <Route path={ROUTES.SIGNUP} element={<SignupPage />} />
+                <Route path={ROUTES.JOURNEY_SETUP} element={<JourneySetupPage />} />
+                <Route path={ROUTES.TRAIN_SELECT} element={<TrainSelectPage />} />
+                <Route path={ROUTES.SEAT_MATRIX} element={<SeatMatrixPage />} />
             </Route>
         </Routes>
     )
