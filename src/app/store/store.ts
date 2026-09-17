5fb39d9a79a444e774d9aeb8ca09c26746b1ed5c
MaTuna01 <ma775100@gmail.com>
2026-09-17 16:29:26 +0900

    [#34]- feat:RTK DevTools 연결을 끊어 운영환경 보안성 확보

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -7,6 +7,7 @@ export const store = configureStore({
     // 리듀서
     reducer: {
         // 로그인 토큰, 유저 정보
+        // prepareHeaders가 이 'user' 키 이름에 의존
         user: userSlice.reducer,
         // 열차 탑승 상태, 착석 상태 및 착석 좌석 정보
         journey: journeySlice.reducer,
@@ -21,6 +22,8 @@ export const store = configureStore({
             userApi.middleware,
             trainApi.middleware
         ),
+    //운영 환경에서는 Redux DevTool를 통해 store에 접근할 수 없도록 연결을 끊음(보안상 토큰 탈취를 막기 위함)
+    devTools: import.meta.env.Dev,
 });
 
 //
