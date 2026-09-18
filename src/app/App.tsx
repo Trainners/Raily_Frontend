88225da4879b5320cdd63e5f87e99f572fc9d2bd
MaTuna01 <ma775100@gmail.com>
2026-09-18 11:13:31 +0900

    [#38]- feat: 라우트 보호 가드를 적용한 중첩 라우트 적용

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,17 +8,25 @@ import {TrainSelectPage} from "../pages/train-select";
 import {SeatMatrixPage} from "../pages/seat-matrix";
 import {SignUpPage} from "../pages/signup";
 import { SettingsPage } from "../pages/settings";
+import {RedirectIfAuthenticated, RequireAuth} from "./router";
 
 export default function App() {
     return (
         <Routes>
+            {/*모든 화면의 공통 껍데기*/}
             <Route element={<ScreenShell />}>
-                <Route path={ROUTES.LOGIN} element={<LoginPage />} />
-                <Route path={ROUTES.SIGNUP} element={<SignUpPage />} />
-                <Route path={ROUTES.JOURNEY_SETUP} element={<JourneySetupPage />} />
-                <Route path={ROUTES.TRAIN_SELECT} element={<TrainSelectPage />} />
-                <Route path={ROUTES.SEAT_MATRIX} element={<SeatMatrixPage />} />
-                <Route path={ROUTES.SETTINGS} element={<SettingsPage />} />
+                {/*공개화면 : 이미 로그인 되어있는 상태라면 여정 검색 화면으로 강제 라우팅*/}
+                <Route element={<RedirectIfAuthenticated/>}>
+                    <Route path={ROUTES.LOGIN} element={<LoginPage />} />
+                    <Route path={ROUTES.SIGNUP} element={<SignUpPage />} />
+                </Route>
+                {/*보호 화면 : 비로그인이면 로그인 화면으로 강제 라우팅*/}
+                <Route element={<RequireAuth/>}>
+                    <Route path={ROUTES.JOURNEY_SETUP} element={<JourneySetupPage />} />
+                    <Route path={ROUTES.TRAIN_SELECT} element={<TrainSelectPage />} />
+                    <Route path={ROUTES.SEAT_MATRIX} element={<SeatMatrixPage />} />
+                    <Route path={ROUTES.SETTINGS} element={<SettingsPage />} />
+                </Route>
             </Route>
         </Routes>
     )
