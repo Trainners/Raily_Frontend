4dff66abf981c03d1519e751aa2bf9ef6c0260f0
dkk011 <dkk011@naver.com>
2026-09-21 11:44:55 +0900

    [#70] - feat: 열차 목록 스켈레톤 추가

--- src/widgets/train-list/index.ts
+++ src/widgets/train-list/index.ts
@@ -1,2 +1,4 @@
 export { default as TrainList } from './ui/TrainList';
 export type { TrainListProps } from './ui/TrainList';
+export { default as TrainListSkeleton } from './ui/TrainListSkeleton';
+export type { TrainListSkeletonProps } from './ui/TrainListSkeleton';
